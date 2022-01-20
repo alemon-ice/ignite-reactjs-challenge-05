dc673b39599a16d02ed01086060785f4f18751a4
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2022-01-19 21:55:52 -0300

    add global styles and create Header and Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import { GetStaticProps } from 'next';
+// import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
-import { getPrismicClient } from '../services/prismic';
+// import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,9 +27,71 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <div className={styles.posts}>
+          <Link href="/">
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div>
+                <div className={styles.createdAt}>
+                  <FiCalendar />
+                  <time>15 Mar 2021</time>
+                </div>
+                <div className={styles.authorName}>
+                  <FiUser />
+                  <span>Joseph Oliveira</span>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div>
+                <div className={styles.createdAt}>
+                  <FiCalendar />
+                  <time>15 Mar 2021</time>
+                </div>
+                <div className={styles.authorName}>
+                  <FiUser />
+                  <span>Joseph Oliveira</span>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div>
+                <div className={styles.createdAt}>
+                  <FiCalendar />
+                  <time>15 Mar 2021</time>
+                </div>
+                <div className={styles.authorName}>
+                  <FiUser />
+                  <span>Joseph Oliveira</span>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <button type="button" className={styles.loadMorePosts}>
+            Carregar mais posts
+          </button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
